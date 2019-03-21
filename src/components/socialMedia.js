a98840090971786210f819b911b002cfd8461f4c
Henrique Pereira Batista <h.p.batista20@gmail.com>
2019-03-21 10:01:40 +0000

    media and data uploaded, icons on landing page displayed

--- src/components/socialMedia.js
+++ src/components/socialMedia.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import mediaList from "../data/socialMedia.json"
+// import mediaList from "../data/socialMedia.json"
+
+import linkedinLogo from "../images/icons/linkedin.svg"
+import githubLogo from "../images/icons/github.svg"
+import emailLogo from "../images/icons/email.svg"
+import flickrLogo from "../images/icons/flickr.svg"
+
 
 const SocialMedia = () => (
     
@@ -7,19 +13,35 @@ const SocialMedia = () => (
         <div>
             <h1>Lorem Ipsum</h1>
             <div className="icon-container">
-               { mediaList.map(icon => (
+                <a href="https://www.linkedin.com/henrique-batista/">
+                    <img src={linkedinLogo} alt="external link to my linkedin account" >       
+                    </img>
+                </a>   
+                <a href="https://github.com/RiqueBR">
+                    <img src={githubLogo} alt="external link to my github account" >       
+                    </img>
+                </a>   
+                <a href="mailto:h.p.batista@outlook.com">
+                    <img src={emailLogo} alt="Send me an email to my personal account" >       
+                    </img>
+                </a>   
+                <a href="https://www.google.com">
+                    <img src={flickrLogo} alt="external link to my flicker account" >       
+                    </img>
+                </a>   
+               
+               
+               {/* { mediaList.map(icon => (
                   
                     <img key={icon.src} src={icon.src}
                         alt={`External link to my ${icon.name} account`} 
                         className="img-responsive" 
                         height="30px"
                         width="30px"    
-                        />
-               ))}
+                        />))} */}
+
             </div>
         </div>
     );
 
 export default SocialMedia;
-
-
